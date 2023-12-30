a333ce7e503ee8566f682bff607e7cebb1698767
DaniloDev18 <154273684+DaniloDev18@users.noreply.github.com>
2023-12-29 23:10:34 -0300

    Delete Dia 3 directory

--- Dia 3/index.js
+++
@@ -1,8 +0,0 @@
-let  nome = 'Danilo Araujo'
-console.log(nome)
-nome = 'Danilo José'
-console.log(nome)
-console.log("==============CONSTANTE===========")
-const NomeCompleto = 'Danilo Jose de Souza Araujo'
-console.log(NomeCompleto)
-
