43863cd57588919d195ca21411b31b5c0bea79ec
Danilo Araujo <dj.araujo027@gmail.com>
2023-12-29 23:12:01 -0300

    Testando Variáveis (let e const)

--- Dia 3/index.js
+++ Dia 3/index.js
@@ -2,10 +2,7 @@ let  nome = 'Danilo Araujo'
 console.log(nome)
 nome = 'Danilo José'
 console.log(nome)
-console.log( "    ")
-
 console.log("==============CONSTANTE===========")
 const NomeCompleto = 'Danilo Jose de Souza Araujo'
-console.log("  ")
 console.log(NomeCompleto)
 
