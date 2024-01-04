b66dce1c8c5c3b0a1ace64e77c3dc86e476db3ba
Danilo Araujo <dj.araujo027@gmail.com>
2024-01-04 10:15:19 -0300

    Aprendendo SwitchCase

---
+++ Dia 5/index.js
@@ -0,0 +1,15 @@
+let Idade = Number(prompt("Insira sua idade"))
+
+switch(Idade){
+    case 10:
+        console.log("Voê tem 10 anos")
+        break
+        
+    case 15:
+        console.log("Você tem 15 anos")
+        break;
+        
+    default:
+        console.log("Default")
+        break;
+}
