bf3bba55c095ebab3c60b98cc3a69a6f44f5e3e2
Danilo Araujo <dj.araujo027@gmail.com>
2024-01-07 22:32:05 -0300

    Aprendendo LOOP (for) + exercicio

---
+++ Dia 6/index.js
@@ -0,0 +1,12 @@
+let valor = Number(prompt("insira um valor"))
+
+for (let contador = 0; contador <= valor; contador++ ) {
+    console.log(contador)
+    break;
+}
+
+//Segunda parte do exercício:
+
+for (let contador2 = 0; contador2 <= 50; contador2+= 5 ) {
+    console.log(contador2)
+}
